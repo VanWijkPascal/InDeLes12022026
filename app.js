a7705ef633946cd5efaf859e6ed2fff476b2ef9f
VivesPascalVanWijk <pascal.vanwijk@student.vives.be>
2026-02-05 16:46:51 +0100

    Add error routing

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 this is some code to set up the usings at the beginning of the File(using the express nodes and such)
 
 this is code for routes
+
+this is code for error handeling (like the 404 handeling)
